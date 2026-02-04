fc7c1d337e4fbc9c1830cea54219b6df24248dfd
Hassan Muhammad Yousuf <alih3766@gmail.com>
2026-02-04 14:18:25 +0100

    DB Instance with complex queries and expressions

--- ground/views.py
+++ ground/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
-from store.models import Product, OrderItem
+from django.db.models.aggregates import Count,Max,Min,Avg
+# from django.db.models import Value, F, Func
+# from django.db.models.functions import Concat
+from store.models import Product, Customer
 
 
 # Create your views here.
 
 def farm(request):
-    queryset =  Product.objects.filter(
-        id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
-
-    return render(request, "index.html", {"name": "Hassan", "products":list(queryset)})
+    queryset = Customer.objects.annotate(
+        orders_count = Count('order')
+    )
+        
+        
+        
+    # queryset = Customer.objects.annotate(
+    #     full_name = Func(F('first_name'), Value(' '), F('last_name'), function= 'CONCAT')
+    # )
+   
+    # queryset = Customer.objects.annotate(
+    #     full_name= Concat('first_name', Value(' '), 'last_name')
+    # )
+    #queryset = Customer.objects.annotate(new_id = F('id') + 1)
+    # result = Product.objects.filter(collection__id=1).aggregate(count = Count('id'), min_price = Min('unit_price'))
+    # queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    # queryset =  Product.objects.prefetch_related('promotions').select_related('collection').all()
+    
+    return render(request, "index.html", {"name": "Hassan", 'result' : list(queryset)})
