2878f46258b00029e221ecc349cc1bad3c5d4f05
Hassan Muhammad Yousuf <alih3766@gmail.com>
2026-02-12 03:32:57 +0100

    Django RestFramework Serializing Related Fields

--- store/serializers.py
+++ store/serializers.py
@@ -1,13 +1,31 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Collection
 from decimal import Decimal
 
+
+# class CollectionSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length = 255)
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length = 255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source = 'unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name= 'calculate_tax')
+    collection = serializers.HyperlinkedRelatedField(
+        queryset = Collection.objects.all(),
+        view_name = 'collection-detail'
+    )
+    
+    
+    # collection = CollectionSerializer()
+    
+    
+    # collection = serializers.StringRelatedField()
+    
+    # collection = serializers.PrimaryKeyRelatedField(
+    #     queryset = Collection.objects.all()
+    # )
     
     
     def calculate_tax(self, product: Product):
