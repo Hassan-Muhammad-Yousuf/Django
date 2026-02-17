b3275bc84bc09e2db9e0d37058d69e0ced171eea
Hassan Muhammad Yousuf <alih3766@gmail.com>
2026-02-17 18:10:23 +0100

    Django RestFramework Class-based Views

--- store/urls.py
+++ store/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("products/", views.product_list),
-    path("products/<int:id>/", views.product_detail),
+    path("products/", views.ProductList.as_view()),
+    #  path("products/", views.product_list),
+    path("products/<int:id>/", views.ProductDetails.as_view()),
+    #  path("products/<int:id>/", views.product_detail),
     path('collections/', views.collection_list),
     path("collections/<int:pk>/", views.collection_details, name='collection-detail'),
 ]
