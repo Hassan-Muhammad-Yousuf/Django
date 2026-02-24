ea6ed96d396ed4c716a6eba19ec5b7b32c053f07
Hassan Muhammad Yousuf <alih3766@gmail.com>
2026-02-24 14:24:19 +0100

    Designing Orders API Returning the Created Order

--- store/serializers.py
+++ store/serializers.py
@@ -138,6 +138,7 @@ class CreateOrderSerializer(serializers.Serializer):
             OrderItem.objects.bulk_create(order_items)
             
             Cart.objects.filter(pk = cart_id).delete()
+            return order
 
     # def create(self, validated_data):
     #     product = Product(**validated_data)
